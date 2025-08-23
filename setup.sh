53bc824dd1ec520f5a3620f430b5dfc9d05acb3d
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-08-23 11:41:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 rm -rf $HOME/.config/nvim
 mkdir -p $HOME/.config/nvim
-git clone https://github.com/LazyVim/starter $HOME/.config/nvim
+git clone https://github.com/LazyVim/starter $HOME/.config/nvim --depth 1
 rm -rf $HOME/.config/nvim/.git
