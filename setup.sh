f21b6fa4d28ada9f780575a95901b2ad37aad75b
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-08-23 13:05:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ rm -rf $HOME/.config/nvim
 mkdir -p $HOME/.config/nvim
 git clone https://github.com/LazyVim/starter $HOME/.config/nvim --depth 1
 rm -rf $HOME/.config/nvim/.git
+
+
+curl https://raw.githubusercontent.com/chubbyhippo/lazyvim-settings/refs/heads/main/lua/config/keymaps.lua -o $HOME/.config/nvim/lua/config/keymaps.lua
