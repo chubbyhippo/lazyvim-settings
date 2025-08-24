fa6079d3bd69b83db6574d4e8570e6307c93dd0f
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-08-24 17:19:38 +0700

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,7 +8,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<Leader><Leader>", "<C-^>", { noremap = true })
+vim.keymap.set("n", "<Leader><Leader>", "<C-^>")
 
 vim.keymap.set("n", "<Leader>ap", function()
   Snacks.explorer.reveal()
